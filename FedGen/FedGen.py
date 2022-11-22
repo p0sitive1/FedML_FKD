bc80bd2c00f218739a5a8a34dc017bac2f3a2c12
P0sitive <andrewshi2002@163.com>
2022-11-22 21:07:37 +0800

    main files

---
+++ FedGen/FedGen.py
@@ -0,0 +1,128 @@
+import torch
+import torchvision
+import torchvision.datasets as datasets
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class LinearNet(nn.Module):
+    """
+    Simple linear net model
+    """
+    def __init__(self):
+        super(LinearNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
+class FedGen(nn.Module):
+    def __init__(self):
+        super(FedGen, self).__init__()
+        self.latent_layer_idx = -1
+        self.hidden_dim, self.latent_dim, self.input_channel, self.n_class, self.noise_dim = 256, 32, 1, 10, 32
+        input_dim = self.noise_dim + self.n_class
+        self.fc_configs = [input_dim, self.hidden_dim]
+
+        self.crossentropy_loss=nn.NLLLoss(reduce=False) # same as above
+        self.diversity_loss = DiversityLoss(metric='l1')
+        self.dist_loss = nn.MSELoss()
+
+        self.fc_layers = nn.ModuleList()
+        for i in range(len(self.fc_configs) - 1):
+            input_dim, out_dim = self.fc_configs[i], self.fc_configs[i + 1]
+            print("Build layer {} X {}".format(input_dim, out_dim))
+            fc = nn.Linear(input_dim, out_dim)
+            bn = nn.BatchNorm1d(out_dim)
+            act = nn.ReLU()
+            self.fc_layers += [fc, bn, act]
+        self.representation_layer = nn.Linear(self.fc_configs[-1], self.latent_dim)
+        print("Build last layer {} X {}".format(self.fc_configs[-1], self.latent_dim))
+    
+    def forward(self, labels, latent_layer_idx=-1, verbose=True):
+        """
+        G(Z|y) or G(X|y):
+        Generate either latent representation( latent_layer_idx < 0) or raw image (latent_layer_idx=0) conditional on labels.
+        :param labels:
+        :param latent_layer_idx:
+            if -1, generate latent representation of the last layer,
+            -2 for the 2nd to last layer, 0 for raw images.
+        :param verbose: also return the sampled Gaussian noise if verbose = True
+        :return: a dictionary of output information.
+        """
+        result = {}
+        batch_size = labels.shape[0]
+        eps = torch.rand((batch_size, self.noise_dim)) # sampling from Gaussian
+        if verbose:
+            result['eps'] = eps
+        if self.embedding: # embedded dense vector
+            y_input = self.embedding_layer(labels)
+        else: # one-hot (sparse) vector
+            y_input = torch.FloatTensor(batch_size, self.n_class)
+            y_input.zero_()
+            #labels = labels.view
+            y_input.scatter_(1, labels.view(-1,1), 1)
+        z = torch.cat((eps, y_input), dim=1)
+        ### FC layers
+        for layer in self.fc_layers:
+            z = layer(z)
+        z = self.representation_layer(z)
+        result['output'] = z
+        return result
+
+
+class DiversityLoss(nn.Module):
+    """
+    Diversity loss for improving the performance.
+    """
+    def __init__(self, metric):
+        """
+        Class initializer.
+        """
+        super().__init__()
+        self.metric = metric
+        self.cosine = nn.CosineSimilarity(dim=2)
+
+    def compute_distance(self, tensor1, tensor2, metric):
+        """
+        Compute the distance between two tensors.
+        """
+        if metric == 'l1':
+            return torch.abs(tensor1 - tensor2).mean(dim=(2,))
+        elif metric == 'l2':
+            return torch.pow(tensor1 - tensor2, 2).mean(dim=(2,))
+        elif metric == 'cosine':
+            return 1 - self.cosine(tensor1, tensor2)
+        else:
+            raise ValueError(metric)
+
+    def pairwise_distance(self, tensor, how):
+        """
+        Compute the pairwise distances between a Tensor's rows.
+        """
+        n_data = tensor.size(0)
+        tensor1 = tensor.expand((n_data, n_data, tensor.size(1)))
+        tensor2 = tensor.unsqueeze(dim=1)
+        return self.compute_distance(tensor1, tensor2, how)
+
+    def forward(self, noises, layer):
+        """
+        Forward propagation.
+        """
+        if len(layer.shape) > 2:
+            layer = layer.view((layer.size(0), -1))
+        layer_dist = self.pairwise_distance(layer, how=self.metric)
+        noise_dist = self.pairwise_distance(noises, how='l2')
+        return torch.exp(torch.mean(-noise_dist * layer_dist))
---
+++ model/serverFKD.py
@@ -0,0 +1,42 @@
+import logging
+
+from .fkd_server_manager import FedMLServerManager
+from .fkd_server_aggregator import FedMLAggregator
+from .default_aggregator import DefaultServerAggregator
+
+
+def fedavg_cross_device(args, process_id, worker_number, comm, device, test_dataloader, model, server_aggregator=None):
+    logging.info("test_data_global.iter_number = {}".format(test_dataloader.iter_number))
+
+    if process_id == 0:
+        init_server(args, device, comm, process_id, worker_number, model, test_dataloader, server_aggregator)
+
+def init_server(args, device, comm, rank, size, model, test_dataloader, aggregator):
+    if aggregator is None:
+        aggregator = DefaultServerAggregator(model, args)
+    aggregator.set_id(-1)
+
+    td_id = id(test_dataloader)
+    logging.info("test_dataloader = {}".format(td_id))
+    logging.info("test_data_global.iter_number = {}".format(test_dataloader.iter_number))
+
+    worker_num = size
+    aggregator = FedMLAggregator(test_dataloader, worker_num, device, args, aggregator)
+
+    # start the distributed training
+    backend = args.backend
+    server_manager = FedMLServerManager(args, aggregator, comm, rank, size, backend)
+    if not args.using_mlops:
+        server_manager.start_train()
+    server_manager.run()
+
+class ServerFKD():
+    def __init__(self, args, device, test_dataloader, model, server_aggregator=None):
+        if args.federated_optimizer == "FedAvg":
+            logging.info("test_data_global.iter_number = {}".format(test_dataloader.iter_number))
+
+            fedavg_cross_device(
+                args, 0, args.worker_num, None, device, test_dataloader, model, server_aggregator=server_aggregator
+            )
+        else:
+            raise Exception("Exception")
